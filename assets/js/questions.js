28fb9e21743d1b181549cc6d2a2813ff21dff92d
Edith <edithlinpy@gmail.com>
2023-01-22 06:23:30 +0000

    add html, css, js files

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+const quesArray = [{
+    question: "Commonly used data types DO NOT include:",
+    options: ["strings","booleans","alerts","numbers"], 
+    answer: 2
+}, 
+{
+    question: "Which one is the correct way to add a comment in a JavaScript?",
+    options: ["** My comment", "<!-- My comment -->", "// My comment", "'My comment"],
+    answer: 2
+},
+{
+    question: "Where should we place an external script reference in an HTML page?",
+    options: ["<head> section", "<body> section", "<footer> section", "<head> or <body> sections"],
+    answer: 3
+},
+{
+    question: "Which one is the corret way to output a message to the web console?",
+    options: ["console.log()", "console.output()","window.log()","console.show()"],
+    answer: 0
+},
+{   
+    question: "Which of the following comparing statement returns false?",
+    options: ["2 < 12", "2 < \"12\"", "2 < \"John\"", "\"2\" > \"12\""],
+    answer: 2
+}];
+
+
+
